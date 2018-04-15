86c02b90f7fb357b1e5578edd3c82dc04b61c895
Igor Batov <igbatov@gmail.com>
2018-04-15 18:04:32 +0300

    final

--- PhoneProcess.php
+++ PhoneProcess.php
@@ -4,126 +4,87 @@ class PhoneProcess {
   const SEVEN = 7;
   const TEN = 10;
   const ELEVEN = 11;
+  const GREEDY_ARRAY_LIMIT = 400;
 
-  public function extractPhones($str) {
-    // сначала попробуем разбить строку на слова
+  public function strictParse($str) {
     $words = $this->splitOnWords($str);
-
-    // Каждое слово будем разбивать на множесто возможных комбинаций телефонных номеров
-    $numbers = [];
+    $phones = [];
     foreach ($words as $word) {
-      $partitions = $this->getPartitions($word);
-      if (count($partitions) === 1) {
-        $numbers = array_merge($numbers, reset($partitions));
+      // удаляем все пробелы, "-", "(", ")", "+"
+      // если то что получилось это число длиной 7, 10 или 11, то ок.
+      // если нет, говорим что мы эту строку распарсить не можем
+      $phone = preg_replace("/[+|\s|\-|\(|\)]/", '', $word);
+      $phoneLen = strlen($phone);
+      if (is_numeric($phone) && ($phoneLen === self::SEVEN || $phoneLen === self::TEN || $phoneLen === self::ELEVEN)) {
+        $phones[] = $phone;
       } else {
-        // если какое-то слово не смогли однозначно разбить на номера,
-        // просто скажем что не справились со всей строкой
         return false;
       }
     }
 
-    // Преобразуем все номера к единому виду
-    foreach ($numbers as $key => $number) {
-      $numLen = strlen($number);
-      if ($numLen === self::SEVEN) {
-        // это городской
-        $numbers[$key] = '8495'.$number;
-      } elseif ($numLen === self::TEN) {
-        $numbers[$key] = '8' . $number;
-      } elseif ($numLen === self::ELEVEN && substr($number, 0, 1) === '7') {
-        $numbers[$key] = '8' . substr($number, 0, 1);
+    foreach ($phones as $k => $phone) {
+      $phones[$k] = $this->normalizePhone($phone);
+    }
+
+    return  array_unique($phones);
+  }
+
+  public function greedyParse($str) {
+    $str = preg_replace("/[^0-9]/", ' ', trim($str));
+    $numbers = preg_split("/\s+/", $str);
+
+    if (count($numbers) > self::GREEDY_ARRAY_LIMIT) {
+      throw new Exception('Cannot parse string with more that '.self::GREEDY_ARRAY_LIMIT.' numbers');
+    }
+
+    $phones = [];
+    while(!empty($numbers)) {
+      $phones = array_merge($phones, $this->getPhones($numbers));
+      array_shift($numbers);
+    }
+
+    foreach ($phones as $k => $phone) {
+      $phones[$k] = $this->normalizePhone($phone);
+    }
+
+    return array_unique($phones);;
+  }
+
+  private function getPhones($numbers) {
+    $phones = [];
+    $phone = "";
+    foreach ($numbers as $number) {
+      $phone .= $number;
+      $phoneLen = strlen($phone);
+      if ($phoneLen === self::SEVEN || $phoneLen === self::TEN || $phoneLen === self::ELEVEN) {
+        $phones[] = $phone;
+      }
+      if ($phoneLen >  self::ELEVEN) {
+        return $phones;
       }
     }
 
-    return $numbers;
+    return $phones;
   }
-
   /**
-   * Считаем что разделителем слов могут быть  ",", ";", "/", "\" с любым кол-вом пробелов после
-   * Самый плохой случай, когда для разделения и внтури телефонного номера
-   * и между нимим используется только пробел
+   * Считаем что разделителем слов могут быть  ",", ";" с любым кол-вом пробелов после
    * @param $str
    * @return array
    */
   public function splitOnWords($str) {
-    return preg_split('!([,|;|/|\\\]\s*)!', $str);
+    return preg_split('!([,|;|]\s*)!', $str);
   }
 
-  public function getPartitions($str) {
-    $str = preg_replace("/[^0-9]/", ' ', $str);
-    $blocks = preg_split("/\s+/", $str);
-    $digitsInBlocksCnt = $this->getDigitCount($blocks);
-    $partitions = []; //здесь будет лежать массив разбиений на номера
-    $partitionsStack = []; // это стэк неоконченных разбиений
-    foreach ($this->getNumbers($blocks) as $number) {
-      $partitionsStack[] = [$number];
+  public function normalizePhone($phone){
+    $phoneLen = strlen($phone);
+    if ($phoneLen === self::SEVEN) {
+      // это городской
+      $phone = '8495'.$phone;
+    } elseif ($phoneLen === self::TEN) {
+      $phone = '8' . $phone;
+    } elseif ($phoneLen === self::ELEVEN && substr($phone, 0, 1) === '7') {
+      $phone = '8' . substr($phone, 0, 1);
     }
-    while (!empty($partitionsStack)) {
-      $partition = array_pop($partitionsStack);
-      $digitInNumbersCnt = $this->getDigitCount($partition);
-
-      // если кол-во цифр в нашем разбиении равно общем кол-ву в строке,
-      // значит мы уже сделали окончательное разбиение
-      if ($digitInNumbersCnt === $digitsInBlocksCnt) {
-        $partitions[] = $partition;
-        continue;
-      }
-
-      // если нет, продолжим разбивать дальше
-      $restBlocks = $this->getBlocksAfterNDigits($blocks, $digitInNumbersCnt);
-      foreach ($this->getNumbers($restBlocks) as $number) {
-        $newPartition = $partition;
-        $newPartition[] = $number;
-        $partitionsStack[] = $newPartition;
-      }
-    }
-
-    return $partitions;
-  }
-
-  /**
-   * Возвращает массив чисел из $blocks без первых $n цифр
-   * @param $blocks - массив чисел
-   * @param int $n - сколько цифр надо скипнуть из $blocks
-   * @return mixed
-   */
-  private function getBlocksAfterNDigits($blocks, $n) {
-    $removedBlocksLength = 0;
-    while ($removedBlocksLength < $n) {
-      $removedBlocksLength += strlen(array_shift($blocks));
-    }
-    return $blocks;
-  }
-
-  private function getDigitCount($arrayOfNumbers) {
-    $numbersDigitCnt = 0;
-    foreach ($arrayOfNumbers as $number) {
-      $numbersDigitCnt += strlen($number);
-    }
-    return $numbersDigitCnt;
-  }
-
-  /**
-   * По массиву чисел $blocks возвращает массив телефонных номеров
-   * которые удалось выделить из первых элементов $blocks
-   * @param $blocks
-   * @return array|bool
-   */
-  private function getNumbers($blocks){
-    $numbers = [];
-    $number = "";
-    $numberLength = 0;
-    while (
-        $numberLength <= self::ELEVEN &&
-        !empty($blocks)
-    ) {
-      $block = array_shift($blocks);
-      $number .= $block;
-      $blockLength = strlen($number);
-      if ($blockLength === self::SEVEN || $blockLength === self::TEN || $blockLength === self::ELEVEN) {
-        $numbers[] = $number;
-      }
-    }
-    return $numbers;
+    return $phone;
   }
 }
--- PhoneProcessTest.php
+++ PhoneProcessTest.php
@@ -3,24 +3,92 @@
 include "PhoneProcess.php";
 
 $tests = [
-    ['input'=>'8 (905) 123 45 67', 'expected' =>["89051234567"]],
-    ['input'=>'9051234567', 'expected' =>["89051234567"]],
-    ['input'=>'123-45-67', 'expected' =>["84951234567"]],
-    ['input'=>'495 123-45-67, 8 (905) 123 45 67, 9051234567', 'expected' =>["84951234567", "89051234567", "89051234567"]],
+    [
+        'input'=>'8 (905) 123 45 67',
+        'expectedStrict' =>["89051234567"],
+        'expectedGreedy' => [
+            0 => '84958905123',
+            1 => '89051234567',
+            3 => '84951234567',
+        ]
+    ],
+
+    [
+        'input'=>'9051234567',
+        'expectedStrict' =>["89051234567"],
+        'expectedGreedy' => [
+            0 => '89051234567',
+        ]
+    ],
+
+    [
+        'input'=>'123-45-67',
+        'expectedStrict' =>["84951234567"],
+        'expectedGreedy' => [
+            0 => '84951234567',
+        ]
+    ],
+
+    [
+        'input'=>'495 123-45-67, 8 (905) 123 45 67, 9051234567',
+        'expectedStrict' =>["84951234567", "89051234567"],
+        'expectedGreedy' =>[
+            0 => '84951234567',
+            1 => '49512345678',
+            3 => '12345678905',
+            4 => '45678905123',
+            5 => '67890512345',
+            6 => '84958905123',
+            7 => '89051234567',
+        ]
+    ],
+
+    [
+        'input'=>'123-45-67 доб. 2200, 8 (905) 123 45 67, 9051234567',
+        'expectedStrict' =>false,
+        'expectedGreedy' =>[
+            0 => '84951234567',
+            1 => '12345672200',
+            2 => '84956722008',
+            3 => '86722008905',
+            4 => '22008905123',
+            5 => '84958905123',
+            6 => '89051234567',
+        ]
+    ],
+
+    [
+        'input'=>'123-45-67 доб. 2200 спросить Машу, 8 (905) 222 3344 - звонить до 18, 9051234567, до 14го числа',
+        'expectedStrict' =>false,
+        'expectedGreedy' =>[
+            0 => '84951234567',
+            1 => '12345672200',
+            2 => '84956722008',
+            3 => '86722008905',
+            4 => '22008905222',
+            5 => '84958905222',
+            6 => '89052223344',
+            8 => '84952223344',
+            9 => '89051234567',
+        ]
+    ],
 ];
 
 foreach ($tests as $test) {
-  runTest($test['input'], $test['expected']);
+  runTest($test['input'], $test['expectedStrict'], $test['expectedGreedy']);
 }
 
 /**
  * @param string $input
  * @param array $expected
  */
-function runTest($input, $expected) {
+function runTest($input, $expectedStrict, $expectedGreedy) {
   $pp = new PhoneProcess();
-  echo $input." - ";
-  $partitions = $pp->extractPhones($input);
-  echo json_encode($partitions) === json_encode($expected) ? "OK" : var_export($partitions, true);
+  echo $input."\n";
+  $phones = $pp->strictParse($input);
+  echo " - strict - ".(json_encode($phones) === json_encode($expectedStrict) ? "OK" : var_export($phones, true));
+  echo "\n";
+  $phones = $pp->greedyParse($input);
+  echo " - greedy - ".(json_encode($phones) === json_encode($expectedGreedy) ? "OK" : var_export($phones, true));
   echo "\n";
 }
